55799cc8526f89e6c922f1494d8f47d8ff850f05
Mia Tran <maiptran157@gmail.com>
2019-05-22 22:25:47 -0700

    set up webcam and create filter for webcam

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -9,7 +9,6 @@ function getVideo() {
         video: true,
         audio: false
     }).then(localMediaStream => {
-        console.log(localMediaStream);
         video.srcObject = localMediaStream;
         video.play();
     }).catch(err => {
@@ -25,12 +24,73 @@ function paintToCanvas() {
     canvas.height = height;
     return setInterval(() => {
         ctx.drawImage(video, 0, 0, width, height);
+        //take the pixels out
+        let pixels = ctx.getImageData(0, 0, width, height);
+        //mess with them
+        // pixels = redEffect(pixels);
+        // pixels = rgbSplit(pixels);
+        pixels = greenScreen(pixels);
+
+        ctx.globalAlpha = 0.8;
+        //put them back
+        ctx.putImageData(pixels, 0, 0);
     }, 16);
 }
 
 function takePhoto() {
+    //played the sound
     snap.currentTime = 0;
     snap.play();
+    //tke the data out of the canvas
+    const data = canvas.toDataURL('image/jpeg');
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download', 'image');
+    link.innerHTML = `<img src="${data}" alt="" />`;
+    strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        pixels.data[i] = pixels.data[i] + 100;
+        pixels.data[i + 1] = pixels.data[i + 1] - 50;
+        pixels.data[i + 2] = pixels.data[i + 2] * 0.5;
+    }
+    return pixels;
+}
+
+function rgbSplit(pixels) {
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        pixels.data[i - 150] = pixels.data[i];
+        pixels.data[i + 100] = pixels.data[i + 1];
+        pixels.data[i - 150] = pixels.data[i + 2];
+    }
+    return pixels;
+}
+
+function greenScreen(pixels) {
+    const levels = {};
+
+    document.querySelectorAll('.rgb input').forEach((input) => {
+        levels[input.name] = input.value;
+    });
+
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        red = pixels.data[i];
+        green = pixels.data[i + 1];
+        blue = pixels.data[i + 2];
+        alpha = pixels.data[i + 3];
+
+        if (red >= levels.rmin
+            && green >= levels.gmin
+            && blue >= levels.bmin
+            && red <= levels.rmax
+            && green <= levels.gmax
+            && blue <= levels.bmax) {
+            pixels.data[i + 3] = 0;
+        }
+    }
+    return pixels;
 }
 
 getVideo();
